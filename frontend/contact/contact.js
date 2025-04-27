f6d12054e2edd50eb23c0c03113e396c263f0199
kiesh0 <qcraerandio@tip.edu.ph>
2025-04-27 13:06:35 +0800

    Update contact.js
    Removed unnecessary comments

--- frontend/contact/contact.js
+++ frontend/contact/contact.js
@@ -39,33 +39,3 @@ function error(err) {
         alert("Cannot get current location");
     }
 }
-
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// var circle = L.circle([51.508, -0.11], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(map);
-
-// var polygon = L.polygon([
-//     [51.509, -0.08],
-//     [51.503, -0.06],
-//     [51.51, -0.047]
-// ]).addTo(map);
-
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
-
-// map.on('click', onMapClick);
